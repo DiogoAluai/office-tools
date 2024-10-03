b348fc039be096d9715f0ac5b811039d4e4e3b4e
Diogo Aluai <diogo.aluai@mgm-tp.com>
2024-10-03 12:27:00 +0100

    Calculate A4 dimensions

--- printer/office-print.sh
+++ printer/office-print.sh
@@ -1,4 +1,6 @@
 #/usr/bin/env bash
 
+# A4 propotions are: 78 char long (does not depend on character) x 58 lines
+
 # Note that printer will not reset printing column to zero on line change with '\n' alone
-nc -q 0 $PRINTER_IP 9100
+sed 's/$/\r/' | nc -q 0 $PRINTER_IP 9100
--- printer/print-random-cow.sh
+++ printer/print-random-cow.sh
@@ -2,4 +2,4 @@
 
 # sed will add '\r' character at the end of the line, just before '\n'
 # this is needed because printer will not reset printing column to zero on line change with '\n' alone
-cowsay-random $1 | sed 's/$/\r/' | office-print
+cowsay-random $1 | office-print
